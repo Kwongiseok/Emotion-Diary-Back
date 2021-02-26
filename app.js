4c90f69bb162b90df7dc9cef38713f1e768719d9
KwonGiSeok <ri2377@naver.com>
2021-02-26 23:49:34 +0900

    heroku update

--- app.js
+++ app.js
@@ -9,13 +9,16 @@ const port = 3060;
 dotenv.config();
 app.use(
   cors({
-    origin: true, //"http://localhost:3000",
+    origin: "http://localhost:3000",
     credentials: true, // 쿠키도 포함해서 전달.
   })
 );
-app.use(express.json()); // json , urlencoded -> 프론트에서 보낸 데이터를 req.body안에 넣어주는 역할을 한다.
 app.use("/", express.static(path.join(__dirname, "uploads")));
 // __dirname/uploads, path.join을 사용하는 이유 : OS 별로 경로의 표기방법이달라서
+app.use(express.json()); // json , urlencoded -> 프론트에서 보낸 데이터를 req.body안에 넣어주는 역할을 한다.
+app.get("/", (req, res) => {
+  res.status(200).json("hi");
+});
 app.use("/post", postRouter);
 
 app.listen(process.env.PORT || port, () => {
