286fec9632149ba0048f2132a522f2fc1f8a2d3f
KwonGiSeok <ri2377@naver.com>
2021-02-27 14:43:07 +0900

    keyFile

--- routes/post.js
+++ routes/post.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { sentimentAnalysis } = require("../config/googleNL");
 const router = express.Router();
 const cors = require("cors");
-router.post("/score", cors(), async (req, res, next) => {
+router.post("/score", async (req, res, next) => {
   const score = await sentimentAnalysis(req.body.text);
   res.status(200).json(score);
 });
